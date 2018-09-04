34bf451614db6479575e506044b9368fb1998566
Sutthinart Khunvadhana <iakgoog@gmail.com>
2018-09-04 12:58:21 +0700

    Finish Chapter 4. Privacy

--- part1-the-money-example/dollar.spec.ts
+++ part1-the-money-example/dollar.spec.ts
@@ -4,16 +4,14 @@ import 'mocha';
 
 describe('dollar class', () => {
 
-  it('should equal 10', () => {
+  it('should equal 10', () => { // use to.deep.equal to compare objects
     const five: Dollar = new Dollar(5);
-    const product: Dollar = five.times(2);
-    expect(product.amount).to.equal(10);
+    expect(five.times(2)).to.deep.equal(new Dollar(10));
   });
 
   it('should equal 15', () => {
     const five = new Dollar(5);
-    const product: Dollar = five.times(3);
-    expect(product.amount).to.equal(15);
+    expect(five.times(3)).to.deep.equal(new Dollar(15));
   });
 
   it('should equal new Dollar(5)', () => {
