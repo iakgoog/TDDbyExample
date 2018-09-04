c8dc0d68b7afc148b5237fa604235f267581906f
Sutthinart Khunvadhana <sutthinart.khunvadhana@thomsonreuters.com>
2018-09-04 08:40:33 +0700

    Finish Chapter 2. Degenerate Objects

--- the-money-example/C1-multi-currency-money/dollar.spec.ts
+++ the-money-example/C1-multi-currency-money/dollar.spec.ts
@@ -5,9 +5,15 @@ import 'mocha';
 describe('dollar class', () => {
 
   it('should equal 10', () => {
+    const five: Dollar = new Dollar(5);
+    const product: Dollar = five.times(2);
+    expect(product.amount).to.equal(10);
+  });
+
+  it('should equal 15', () => {
     const five = new Dollar(5);
-    five.times(2);
-    expect(five.amount).to.equal(10);
+    const product: Dollar = five.times(3);
+    expect(product.amount).to.equal(15);
   });
 
 });
