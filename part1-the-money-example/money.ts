0ba375e7c6ef3080fde768717d32c1dd67520fa7
Sutthinart Khunvadhana <iakgoog@gmail.com>
2018-09-05 23:13:01 +0700

    Finish Chapter 8. Makin' Objects

--- part1-the-money-example/money.ts
+++ part1-the-money-example/money.ts
@@ -1,12 +1,42 @@
-export class Money {
+export abstract class Money {
+  abstract times(multiplier: number): Money;
+
   protected amount: number;
 
   constructor(amount: number) {
     this.amount = amount;
   }
 
+  static dollar(amount: number): Money {
+    return new Dollar(amount);
+  }
+
+  static franc(amount: number): Franc {
+    return new Franc(amount);
+  }
+
   public equals(object: any): boolean {
     const money = <Money> object;
     return this.amount === money.amount && typeof this === typeof money;
+  }
+}
+
+export class Dollar extends Money {
+  constructor(amount: number) {
+    super(amount);
+  }
+
+  times(multiplier: number): Dollar {
+    return new Dollar(this.amount * multiplier);
+  }
+}
+
+export class Franc extends Money {
+  constructor(amount: number) {
+    super(amount);
+  }
+
+  times(multiplier: number): Franc {
+    return new Franc(this.amount * multiplier);
   }
 }
--- part1-the-money-example/test.spec.ts
+++ part1-the-money-example/test.spec.ts
@@ -1,56 +1,63 @@
-import { Dollar } from './dollar';
-import { Franc } from './franc';
+import { Dollar, Franc, Money } from './money';
 import { expect } from 'chai';
 import 'mocha';
 
 describe('Test Dollar Multiplication', () => {
 
+  let five: Dollar;
+
+  before(() => {
+    five = Money.dollar(5);
+  });
+
   it('Dollar(5).times(2) should equal 10', () => { // use to.deep.equal to compare objects
-    const five: Dollar = new Dollar(5);
-    expect(five.times(2)).to.deep.equal(new Dollar(10));
+    expect(five.times(2)).to.deep.equal(Money.dollar(10));
   });
 
   it('Dollar(5).times(3) should equal 15', () => {
-    const five: Dollar = new Dollar(5);
-    expect(five.times(3)).to.deep.equal(new Dollar(15));
+    expect(five.times(3)).to.deep.equal(Money.dollar(15));
   });
 
 });
 
 describe('Test Franc Multiplication', () => {
 
+  let five: Franc;
+
+  before(() => {
+    five = Money.franc(5);
+  })
+
   it('Franc(5).times(2) should equal 10', () => { // use to.deep.equal to compare objects
-    const five: Franc = new Franc(5);
-    expect(five.times(2)).to.deep.equal(new Franc(10));
+    expect(five.times(2)).to.deep.equal(Money.franc(10));
   });
 
   it('Franc(5).times(3) should equal 15', () => {
-    const five: Franc = new Franc(5);
-    expect(five.times(3)).to.deep.equal(new Franc(15));
+    expect(five.times(3)).to.deep.equal(Money.franc(15));
   });
 
 });
 
 describe('Test Equality', () => {
 
-  it('new Dollar(5) should equal new Dollar(5)', () => {
-    expect(new Dollar(5).equals(new Dollar(5))).to.equal(true);
+  it('Money.dollar(5) should equal Money.dollar(5)', () => {
+    expect(Money.dollar(5).equals(Money.dollar(5))).to.equal(true);
   })
 
-  it('new Dollar(5) should not equal new Dollar(6)', () => {
-    expect(new Dollar(5).equals(new Dollar(6))).to.equal(false);
+  it('Money.dollar(5) should not equal new Dollar(6)', () => {
+    expect(Money.dollar(5).equals(Money.dollar(6))).to.equal(false);
   })
 
-  it('new Franc(5) should equal new Franc(5)', () => {
-    expect(new Franc(5).equals(new Franc(5))).to.equal(true);
+  it('Money.franc(5) should equal Money.franc(5)', () => {
+    expect(Money.franc(5).equals(Money.franc(5))).to.equal(true);
   })
 
-  it('new Franc(5) should not equal new Franc(6)', () => {
-    expect(new Franc(5).equals(new Franc(6))).to.equal(false);
+  it('Money.franc(5) should not equal Money.franc(6)', () => {
+    expect(Money.franc(5).equals(Money.franc(6))).to.equal(false);
   })
 
-  it('new Franc(5) should equal new Dollar(5)', () => {
-    expect(new Franc(5).equals(new Dollar(5))).to.equal(true);
+  it('Money.franc(5) should equal Money.dollar(5)', () => {
+    expect(Money.franc(5).equals(Money.dollar(5))).to.equal(true);
   })
 
 });
